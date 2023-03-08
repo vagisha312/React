78330e85e08a74af1ec04688293737b5a2e0b02e
vagisha312 <108082732+vagisha312@users.noreply.github.com>
2023-03-08 13:22:01 +0530

    added inline style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const CustomStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+
 ReactDOM.render(
   <div>
+   <h1 style={CustomStyle}>Hello World!</h1>,
     <h1>Hello World!</h1>
     <p>This is a paragraph.</p>
   </div>,
